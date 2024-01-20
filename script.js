7a01b45369fa1d31aba8ce23a91000cfe6b0ab18
Victor-Mutwiri <mutwirilincoln19@gmail.com>
2024-01-20 13:38:46 +0300

    1.1

--- script.js
+++ script.js
@@ -22,25 +22,24 @@ let getMovie = ()=>{
                     result.innerHTML = `
                 <div class="info">
                     <img src=${data.Poster} class="poster">
-                </div>
-                <div>
-                    <h2>${data.Title}</h2>
-                    <div class="rating">
-                        <img src="star-icon.svg">
-                        <h4>${data.imdbRating}</h4>
-                    </div>
-                    <div class="details">
-                        <span>${data.Rated}</span>
-                        <span>${data.Year}</span>
-                        <span>${data.Runtime}</span>
-                    </div>
-                    <div class="genre">
-                        <div>${data.Genre.split(",").
-                        join("</div><div>")}
+                    <div>
+                        <h2>${data.Title}</h2>
+                        <div class="rating">
+                            <img src="star-icon.svg">
+                            <h4>${data.imdbRating}</h4>
+                        </div>
+                        <div class="details">
+                            <span>${data.Rated}</span>
+                            <span>${data.Year}</span>
+                            <span>${data.Runtime}</span>
+                        </div>
+                        <div class="genre">
+                            <div>${data.Genre.split(",").
+                            join("</div><div>")}
+                            </div>
                         </div>
                     </div>
                 </div>
-                
                 <h3>Plot:</h3>
                 <p>${data.Plot}</p>
                 <h3>Cast:</h3>
